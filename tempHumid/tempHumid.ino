d6168d2983be47453a9be68751419e3ebde50e36
jasonlin1222 <jasonlin1222@gmail.com>
2019-12-26 16:12:26 +0800

    update temp

--- tempHumid/tempHumid.ino
+++ tempHumid/tempHumid.ino
@@ -1,21 +1,40 @@
 #include <DHT.h>
-#include <DHT_U.h>
-#define dht_apin A0
 
-dht DHT;
-int humid = 0;
-int temp = 0;
+#define DHTPIN 0
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
 }
 
 void loop() {
-  DHT.read11(dht_apin);
-  humid = DHT.humidity;
-  temp = DHT.temperature;
-  Serial.print(humid);
-  Serial.print(" ");
-  Serial.print(temp);
-  delay(5000);
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
 }
