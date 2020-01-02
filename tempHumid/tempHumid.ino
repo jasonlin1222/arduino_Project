51d1ef6c37e285205b2c2feca055eb740a0054d0
Lucas <weil010b@gmail.com>
2020-01-02 08:06:30 +0800

    deleted library cause it was useless

--- tempHumid/tempHumid.ino
+++ tempHumid/tempHumid.ino
@@ -1,11 +1,11 @@
 #include <DHT.h>
 
-#define DHTPIN 0
+#define DHTPIN 2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(9600)
+  Serial.begin(9600);
   dht.begin();
 }
 
