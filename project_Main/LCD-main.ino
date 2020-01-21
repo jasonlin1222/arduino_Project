747c42940dbf618e47de5aa0d93e94128d9c4317
jasonlin1222 <jasonlin1222@gmail.com>
2020-01-22 00:13:40 +0800

    first commit

--- project_Main/LCD-main.ino
+++ project_Main/LCD-main.ino
@@ -1,6 +1,6 @@
 #include <Wire.h> // Library for I2C communication
 #include <LiquidCrystal_I2C.h> // Library for LCD
-#include <DHT.h>
+#include <DHT.h> //Library for DHT module
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
@@ -10,13 +10,6 @@ DHT dht(DHTPIN, DHTTYPE);
 // Wiring: SDA pin is connected to A4 and SCL pin to A5.
 // Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Change to (0x27,16,2) for 16x2 LCD.
-
-
-int encoder0PinA = 5;
-int encoder0PinB = 4;
-int encoder0Pos = 0;
-int encoder0PinALast = LOW;
-int n = digitalRead(encoder0PinA);
 
 void setup() {
   pinMode(Grove_Water_Sensor, 13);
@@ -30,49 +23,15 @@ void setup() {
   dht.begin();
 }
 void loop() {
-    delay(000);
-    
-    if(digitalRead(encoder0PinA) != n){
-        if (digitalRead(encoder0PinB) != digitalRead(encoder0PinA)){
-            encoder0Pos--;
-            lcd.clear();
-        }
-        else {
-            encoder0Pos++;
-            lcd.clear();  
-        }
-    }
-    bool button = digitalRead(3);
-    int spin = encoder0Pos % 3;
-    
+    delay(2000);
+
     String h = (String)dht.readHumidity();
     String t = (String)dht.readTemperature();
     String w = digitalRead(Grove_Water_Sensor) == LOW ? "Yes water":"No water";
 
+    lcd.setCursor(0, 0);
+    lcd.print("Humid: " + h );
     lcd.setCursor(0, 1);
-    lcd.print(spin);
-    switch(spin){
-        case 0:
-            lcd.setCursor(0, 0);
-            lcd.print("Humid: " + h );
-            lcd.setCursor(0, 1);
-            //lcd.print("Temp: "+ t);
-        case 1:
-            lcd.setCursor(0, 0);
-            lcd.print("Water: " + w);
-        case 2:
-            lcd.setCursor(0, 0);
-            lcd.print("press button for pump");
-            
-            if(button){
-                //write relay high
-                lcd.setCursor(0, 1);
-                lcd.print("starting......");
-                delay(10000);
-                digitalWrite(7, HIGH);
-                delay(5000);
-                digitalWrite(7,LOW);
-                //write relay low
-            }
+    lcd.print("Temp: "+ t);
     }
 }
